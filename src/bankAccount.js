6382ec0395f5a661d3256e8d1fc3c1dcd36dd44c
Brijesh Pujara <pujara.brijesh1@gmail.com>
2022-09-21 11:32:00 +0100

    edge cases for empty deposit/withdrawal with tests

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -5,27 +5,33 @@ class BankAccount {
   }
 
   depositAmmount(deposit) {
-    this.amount += deposit;
-    const new_transaction = {
-      deposit: deposit.toFixed(2),
-      withdrawal: 0,
-      balance: this.amount.toFixed(2),
-      date: new Date().toLocaleString(),
-    };
-
-    this.transactions.push(new_transaction);
+    if (deposit == null) {
+      throw new Error("Please enter deposit amount");
+    } else {
+      this.amount += deposit;
+      const new_transaction = {
+        deposit: deposit.toFixed(2),
+        withdrawal: 0,
+        balance: this.amount.toFixed(2),
+        date: new Date().toLocaleString(),
+      };
+      this.transactions.push(new_transaction);
+    }
   }
 
   withdrawalAmmount(withdrawal) {
-    this.amount -= withdrawal;
-    const new_transaction = {
-      deposit: 0,
-      withdrawal: withdrawal.toFixed(2),
-      balance: this.amount.toFixed(2),
-      date: new Date().toLocaleString(),
-    };
-
-    this.transactions.push(new_transaction);
+    if (withdrawal == null) {
+      throw new Error("Please enter withdrawal amount");
+    } else {
+      this.amount -= withdrawal;
+      const new_transaction = {
+        deposit: 0,
+        withdrawal: withdrawal.toFixed(2),
+        balance: this.amount.toFixed(2),
+        date: new Date().toLocaleString(),
+      };
+      this.transactions.push(new_transaction);
+    }
   }
 
   totalTransactions() {
@@ -33,22 +39,28 @@ class BankAccount {
   }
 
   statement() {
-    let total = ["date || credit || debit || balance \n"];
+    return this.#statementList();
+  }
+
+  // Private functions
+
+  #statementList() {
+    this.total = ["date || credit || debit || balance \n"];
     for (let i = 0; i < this.transactions.length; i++) {
       const trans = this.transactions[i];
-      total.push(
+      this.total.push(
         `${trans.date}| ${trans.deposit}| ${trans.withdrawal}| ${trans.balance} \n `
       );
     }
-    return total;
+    return this.total;
   }
 }
 
 module.exports = BankAccount;
 
-const account = new BankAccount();
-account.depositAmmount(1000);
-account.depositAmmount(2000);
-account.withdrawalAmmount(500);
+// const account = new BankAccount();
+// account.depositAmmount();
+// account.depositAmmount(2000);
+// account.withdrawalAmmount(500);
 
-console.log(account.statement());
+// console.log(account.statement());
--- src/bankAccount.test.js
+++ src/bankAccount.test.js
@@ -8,20 +8,23 @@ describe("Bank Account", () => {
     jest.useFakeTimers("modern");
     jest.setSystemTime(new Date("20 Sept 2022 18:00:00 GMT"));
   });
+
   it("should return empty transactions list", () => {
     expect(account.totalTransactions()).toEqual([]);
   });
   it("should deposit 1000 into account", () => {
     account.depositAmmount(1000);
     expect(account.totalTransactions()).toEqual(
-      expect.arrayContaining([expect.objectContaining({ "deposit": "1000.00" })])
+      expect.arrayContaining([expect.objectContaining({ deposit: "1000.00" })])
     );
   });
   it("should withdraw 500 from the account ", () => {
     account.depositAmmount(1000);
     account.withdrawalAmmount(500);
     expect(account.totalTransactions()).toEqual(
-      expect.arrayContaining([expect.objectContaining({ "withdrawal": "500.00" })])
+      expect.arrayContaining([
+        expect.objectContaining({ withdrawal: "500.00" }),
+      ])
     );
   });
 
@@ -29,7 +32,7 @@ describe("Bank Account", () => {
     account.depositAmmount(1000);
     account.withdrawalAmmount(500);
     expect(account.totalTransactions()).toEqual(
-      expect.arrayContaining([expect.objectContaining({ "balance": "500.00" })])
+      expect.arrayContaining([expect.objectContaining({ balance: "500.00" })])
     );
   });
 
@@ -38,6 +41,25 @@ describe("Bank Account", () => {
     account.depositAmmount(1000);
     console.log(account.totalTransactions());
     expect(account.totalTransactions().length).toEqual(2);
+  });
+
+  it("should throw an error when there is no deposit amount entered ", () => {
+    try {
+      account.depositAmmount();
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(error.message).toBe("Please enter deposit amount");
+    }
+  });
+
+  it('should throw an error when there is no withdrawal amount entered ', () => {
+    try {
+      account.withdrawalAmmount();
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(error.message).toBe("Please enter withdrawal amount");
+    }
+    
   });
 
   it("should provide a breakdown of the transaction history formatted ", () => {
